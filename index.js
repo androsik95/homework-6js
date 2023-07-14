5e44518919cc75a2ced72403efc0fc1eee47a39f
androsik95 <gerasimukandrej@gmail.com>
2023-07-14 17:09:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+
+// // Задание 1
+// [Is it even?](https://www.codewars.com/kata/555a67db74814aa4ee0001b5)
+
+// function testEven(n) {
+//     return n % 2 == 0
+//   } 
+
+// // Задание 2
+// [Will you make it?](https://www.codewars.com/kata/5861d28f124b35723e00005e)
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return mpg * fuelLeft >= distanceToPump;
+//   };
+
+// // Задание 3
+// [You Can't Code Under Pressure](https://www.codewars.com/kata/53ee5429ba190077850011d4)
+
+// function doubleInteger(i) {
+//     return i * 2;
+//   }
+
+// // Задание 4
+// [N-th Fibonacci](https://www.codewars.com/kata/522551eee9abb932420004a0)
+
+// function nthFibo(n) {
+//     let arr=[0,1];
+//      for(let i=1; i<n;i++){
+//          arr.push(arr[i-1]+arr[i]);
+//      }
+//      return n > 0 ? arr[n-1] : 0;
+//  }
+
+// // Задание 5
+//  [Are You Playing Banjo?](https://www.codewars.com/kata/53af2b8861023f1d88000832)
+
+// function areYouPlayingBanjo(name) {
+//     if (name[0] == 'R' || name[0] == 'r')
+//        return name + " plays banjo";
+//      else
+//        return name + " does not play banjo";
+   
+//    }
+
+// Задание 6
+//  [reversed-strings](https://www.codewars.com/kata/reversed-strings)
+
+// function solution(str){
+//     const solution = str => str.split('').reverse().join('')
+//     return str.split('').reverse().join('') }
+
+// Задание 7
+//  [palindrome-strings](https://www.codewars.com/kata/palindrome-strings)
+
+// function isPalindrome(line) {
+//     return (String (line)==String (line).split('').reverse().join(''));
+//   }
+
+//  Задание 8
+//   [opposite-number](http://www.codewars.com/kata/opposite-number)
+
+// function opposite(number) { 
+//   return (-number)
+// }
